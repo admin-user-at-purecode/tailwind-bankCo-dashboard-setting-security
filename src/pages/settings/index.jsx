489b907dec0c7bdd8fcd4d08257c1d8b7a209f35
iamsinghshubhamm <iamsinghshubhamm@gmail.com>
2024-03-11 15:57:33 +0530

    varient added

--- src/pages/settings/index.jsx
+++ src/pages/settings/index.jsx
@@ -8,7 +8,7 @@ function Settings() {
       <div className="grid grid-cols-1 xl:grid-cols-12 bg-white dark:bg-darkblack-600 rounded-xl">
         <SettingsSidebar />
         <div className="py-8 px-10 col-span-9 tab-content">
-          <Security />
+       
         </div>
       </div>
     </main>
